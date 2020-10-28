bd56fbb78c1b3f51ce55e11ffb1a7cc7678ad68c
Kuba Jakubowski <kubagp2@gmail.com>
2020-10-28 17:06:20 +0100

    site layout and design

--- analogowy.php
+++ analogowy.php
@@ -5,21 +5,39 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Zegar analogowy</title>
       <link rel="stylesheet" href="analogowy/css/clock.css">
-      <script src=""></script>
+      <link rel="stylesheet" href="analogowy/css/style.css">
    </head>
    <body>
-      <div class="demo-container clocks single local bounce">
-         <article class="clock ios7 show">
-            <div class="hours-container">
-               <div class="hours"></div>
+      <div class="overlay">
+         <a href="/" class="come-back">
+            come back
+         </a>
+      </div>
+      <div class="container">
+         <div class="demo-container clocks single local bounce">
+            <article class="clock ios7 show">
+               <div class="hours-container">
+                  <div class="hours"></div>
+               </div>
+               <div class="minutes-container">
+                  <div class="minutes"></div>
+               </div>
+               <div class="seconds-container">
+                  <div class="seconds"></div>
+               </div>
+            </article>
+         </div>
+      </div>
+      <div class="progress-container">
+         <div class="progress-info">
+            Koniec lekcji za: 5 minut
+         </div>
+         <div class="progress-bar-container">
+            <div class="progress-bar">
+               <div class="progress-filled"></div>
             </div>
-            <div class="minutes-container">
-               <div class="minutes"></div>
-            </div>
-            <div class="seconds-container">
-               <div class="seconds"></div>
-            </div>
-         </article>
+         </div>
       </div>
    </body>
+   <script src="analogowy/js/clock.js"></script>
 </html>
