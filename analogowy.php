bec5df473c0a56e3a444dd8e931ae1fdf95b42cc
Kuba Jakubowski <kubagp2@gmail.com>
2020-10-28 16:07:42 +0100

    copied entire css from some css tutorial website

---
+++ analogowy.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Zegar analogowy</title>
+      <link rel="stylesheet" href="analogowy/css/clock.css">
+      <script src=""></script>
+   </head>
+   <body>
+      <div class="demo-container clocks single local bounce">
+         <article class="clock ios7 show">
+            <div class="hours-container">
+               <div class="hours"></div>
+            </div>
+            <div class="minutes-container">
+               <div class="minutes"></div>
+            </div>
+            <div class="seconds-container">
+               <div class="seconds"></div>
+            </div>
+         </article>
+      </div>
+   </body>
+</html>
